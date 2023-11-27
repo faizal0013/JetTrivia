631ec1da657c96017d02bae704a08f1a0e61607e
Faizal Khalifa <faizalfkhalifa786@gmail.com>
2023-11-27 12:35:37 +0530

    fix CircularProgressIndicator UI

--- app/src/main/java/com/demo/jettrivia/components/Questions.kt
+++ app/src/main/java/com/demo/jettrivia/components/Questions.kt
@@ -5,14 +5,17 @@ import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
@@ -58,14 +61,17 @@ fun Questions(viewModel: QuestionViewModel) {
         mutableStateOf(0)
     }
 
+
     if(viewModel.data.value.loading == true) {
-
         Surface(
-            modifier = Modifier.size(20.dp)
+            modifier = Modifier.fillMaxSize(),
         ) {
-            CircularProgressIndicator()
+            Box (
+                contentAlignment = Alignment.Center
+            ){
+                CircularProgressIndicator()
+            }
         }
-
     }else {
 
         val question = try {
